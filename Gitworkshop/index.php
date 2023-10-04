9f136109133bcc3f62e26218b49fee15ec75d385
Bishesta <bishestasubedi200@gmail.com>
2023-10-04 22:22:57 +0545

    first commit

---
+++ Gitworkshop/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/index.css">
+</head>
+<body>
+    <?php include('templates/header.php'); ?>
+    <!-- <div class="container">
+        <h2>Welcome to Your Project</h2>
+        <p>This is the homepage of your project. You can customize this page with your content and features.</p>
+        <p>If you're new here, you can <a href="pages/signup.php">sign up</a> for an account, or if you already have an account, you can <a href="pages/login.php">log in</a>.</p>
+    </div> -->
+    <?php include('templates/category.php'); ?>
+    <?php include('templates/footer.php'); ?>
+</body>
+</html>
